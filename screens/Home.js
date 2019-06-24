4b05a7b99107220ad2927665e01bf55f05b39782
wellsadam16089 <wellsadam82@gmail.com>
2019-06-23 20:33:21 -0500

    nice UI and functional changes
    6/23/2019

--- screens/Home.js
+++ screens/Home.js
@@ -1,55 +1,104 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, FlatList, Image} from 'react-native';
+import { StyleSheet, View, Text, Button, FlatList, Image, Alert} from 'react-native';
 import { connect } from 'react-redux';
 import { SessionObject } from './Session';
-import { Container, Header, Content, Card, CardItem, Body } from 'native-base';
+import { Card, CardItem, Fab, Separator } from 'native-base';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Icon } from 'react-native-elements';
 //NativeBase, FirstBorn
 
 class Home extends React.Component {
+  static navigationOptions = ({navigation, screenProps}) => ({
+    header: null
+  })
   render() {
       return (
         <View style={styles.container}>
-          <View style={styles.container}>
-            <Text>Home Screen!! :)</Text>
+        <View style={{height: 24}}></View>
+          <View style={styles.horizontal}>
+          <Image source={require('../cards/PokerRecords.png')} style={styles.imageStyle}/>
+          <Image source={require('../cards/record.png')} style={styles.imageStyle}/>
+          </View>
+          <View style={{flex: .3,}}>
+          <Separator bordered style={{backgroundColor: '#fff'}}>
+            <Text style={{textAlign: "center", fontWeight: 'bold'}}>My Sessions</Text>
+          </Separator>
           </View>
           <View style={styles.style_flex3}>
-            <Button title="Add new Session!" onPress={() => 
-              this.addNewSession(this.props.NextId, this.props.SessionList.length + 1) } />
+          { this.FirstTimeButton() }
               <FlatList
                 data={this.props.SessionList}
                 renderItem={({item}) => this.SessionComponent(item)}
                 keyExtractor={(item, index) => index.toString()}
               />
+              <Fab 
+                active={true} 
+                onPress={() => this.addNewSession(this.props.NextId, this.props.NextId + 1)}
+                position="bottomRight">
+                <Text>+</Text>
+              </Fab>
           </View>
         </View>
       );
     }
     SessionComponent = (session) => {
       return (
-        <View style={{paddingLeft: 20, paddingRight: 20}}>
-          <TouchableOpacity
-            onPress={() => { this.NavigateToNextScreen(session) }}>
-              <Card>
-                <CardItem header>
-                  <Text>{'Session ' + session.Name.toString()}</Text>
-                </CardItem>
-              </Card>
-          </TouchableOpacity>
+        <View style={{paddingLeft: 15, paddingRight: 10}}>
+          <View style={styles.horizontal}>
+            <View style={{flex: 9}}>
+              <TouchableOpacity
+                onPress={() => { this.NavigateToNextScreen(session) }}>
+                  <Card>
+                    <CardItem header>
+                      <View style={{flexDirection: "column"}}>
+                        <Text>{'Session ' + session.Name.toString()}</Text>
+                        <Text style={{}}>{'Hands: ' + session.HandList.length}</Text>
+                      </View>
+                    </CardItem>
+                  </Card>
+              </TouchableOpacity>
+            </View>
+            <View style={{height: 50, paddingTop: 20}}>
+              <Button title="X" color='red' onPress={ () => this.DeleteSession(session) }></Button>
+            </View>
+          </View>
         </View>
+      );
+    };
+    FirstTimeButton = () => {
+      if (this.props.SessionList.length == 0) {
+        return(
+          <View>
+            <Card>
+              <CardItem>
+                  <Text>
+                    Click the "+" button in the bottom right to get started!
+                  </Text>
+              </CardItem>
+            </Card>
+          </View>
+        );
+      }
+      else {
+        return (
+          <View>
+          </View>
+        );
+      }
+    };
+    DeleteSession = (session) => {
+      Alert.alert(
+        'Are you sure?',
+        "This will permanently delete the Session and all of it's data",
+        [
+          {text: 'NO'},
+          {text: 'YES', onPress: () => this.props.deleteSession(session)},
+        ],
       );
     };
     addNewSession = (id, name) => {
       var newSession = new SessionObject(id, name, []);
       this.props.addSession(newSession);
-
-      /**<FlatList
-            data={this.props.SessionList}
-            renderItem={({item}) => this.SessionComponent(item)}
-            keyExtractor={(item, index) => index.toString()}
-            /> 
-            <Button title={'Session ' + session.Name.toString()} 
-              onPress={() => { this.NavigateToNextScreen(session) }} />*/
     };
     UpdateSession = (session) => {
       this.props.updateSession(session);
@@ -68,6 +117,15 @@ const styles = StyleSheet.create({
   },
   style_flex3: {
     flex: 3,
+    backgroundColor: '#d3d3d3',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  horizontal: {
+    flexDirection: 'row',
+  },
+  imageStyle: {
+    width: 200, height: 150
   },
 });
 
@@ -90,7 +148,13 @@ const mapDispatchToProps = (dispatch) => {
           type: "UPDATE_SESSION",
           payload: session
         });
-      }
+      },
+      deleteSession: (session) => {
+        dispatch({
+          type: "DELETE_SESSION",
+          payload: session
+        });
+      },
   };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- screens/Session.js
+++ screens/Session.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, Alert, FlatList} from 'react-native';
+import { StyleSheet, View, Text, FlatList, TextInput} from 'react-native';
+import { Button, Card, CardItem, Right, DatePicker } from 'native-base';
 import { HandObject } from './Hand';
+import _Card from '../CardGrabber';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import AntIcon from "react-native-vector-icons/AntDesign";
 
 export function SessionObject(_id, _name, _hands) {
     this.Id = _id, //int
     this.Name = _name, //string
-    this.HandList = _hands //array of HandObject objects
+    this.HandList = _hands, //array of HandObject objects
+    this.Location = "location", //string
+    this.SessionDate = new Date().toLocaleDateString() //string
 }
 var defaultSession = new SessionObject(0, "default", []);
 
 export default class Session extends React.Component {
+  static navigationOptions = ({navigation, screenProps}) => ({
+    title: 'Session ' + (navigation.state.params.Session.Id + 1)
+  })
   state = {
     Session: this.props.navigation.getParam('Session'),
+    EditMode: false,
+    Edit_Name: this.props.navigation.getParam('Session').Name,
+    Edit_Location: this.props.navigation.getParam('Session').Location,
+    Edit_Date: this.props.navigation.getParam('Session').SessionDate,
   }
   render() {
       return (
@@ -19,6 +32,8 @@ export default class Session extends React.Component {
           <View style={styles.container}>
             { this.SessionHeader() }
           </View>
+          { this.LineDivider() }
+          { this.SessionDivider() }
           <View style={styles.style_flex3}>
             { this.SessionBody() }
           </View>
@@ -34,19 +49,101 @@ export default class Session extends React.Component {
           HandList: [...state.Session.HandList, newHand]
         },
         }))
+      this.NavigateToNextScreen(newHand);
     };
     componentDidUpdate() {
         this.props.navigation.state.params.updateMethod(this.state.Session); //update store
     };
     SessionHeader = () => {
+      if (!this.state.EditMode) {
         return (
-            <Text>Session {this.state.Session.Name}</Text>
+          <View style={{paddingLeft: 10, paddingTop: 10, paddingRight: 10}}>
+            <View style={styles.horizontal}>
+                <View style={{alignSelf: 'flex-start', paddingLeft: 10}}>
+                    <Text style={styles.textStyle}>{this.state.Session.Name}</Text>
+                </View>
+                <View style={{alignSelf: 'flex-end', paddingRight: 10}}>
+                  <AntIcon name="edit" color="blue" size={40} 
+                  onPress={() => this.setState((state, props) => ({
+                  ...state,
+                  EditMode: true,
+                  })) } />
+                </View>
+            </View>
+            <Text style={styles.textStyle}>{this.state.Session.Location}</Text>
+            <Text style={styles.textStyle}>{this.state.Session.SessionDate}</Text>
+          </View>
         );
+      }
+      else {
+        return (
+          <View>
+            <View style={{alignSelf: 'flex-end', paddingRight: 10}}>
+              <AntIcon name="check" color="green" size={40} 
+              onPress={() => this.SaveEdits() } />
+            </View>
+            <View style={{ flexDirection: 'row'}}>
+              <Text style={styles.textStyle}>Name: </Text>
+              <TextInput placeholder={this.state.Session.Name.toString()} style={styles.textInput}
+                onChangeText={(text) => this.setState((state, props) => ({
+                    ...state,
+                    Edit_Name: text,
+                    })) } />
+            </View>
+            <View style={{ flexDirection: 'row'}}>
+              <Text style={styles.textStyle}>Location: </Text>
+              <TextInput placeholder={this.state.Session.Location.toString()} style={styles.textInput}
+              onChangeText={(text) => this.setState((state, props) => ({
+                    ...state,
+                    Edit_Location: text,
+                    })) } />
+            </View>
+            <DatePicker style={styles.textStyle} 
+              onDateChange={(text) => this.setState((state, props) => ({
+                    ...state,
+                    Edit_Date: text.toString().substr(4, 12)
+                    })) } />
+          </View>
+        );
+      }
+    };
+    SaveEdits = () => {
+      this.setState((state, props) => ({
+        ...state,
+        EditMode: false,
+        Session: {
+          ...state.Session,
+          Name: state.Edit_Name,
+          Location: state.Edit_Location,
+          SessionDate: state.Edit_Date,
+        }
+        }))
+      };
+    SessionDivider = () => {
+      if (!this.state.EditMode) {
+        return (
+          <View style={styles.horizontal}>
+            <View style={{alignSelf: 'flex-start', paddingLeft: 10}}>
+              <Text style={styles.textStyle}>Hands: {this.state.Session.HandList.length}</Text>
+            </View>
+            <View style={{alignSelf: 'flex-end', paddingRight: 10}}>
+                <Button rounded info onPress={() => this.addItem() }>
+                  <Text style={{paddingLeft: 15, paddingRight: 15, fontSize: 20}}>New Hand</Text>
+                </Button>
+            </View>
+        </View>
+        );
+      }
+      else {
+        return (
+          <View></View>
+        );
+      }
     };
     SessionBody = () => {
+      if (!this.state.EditMode) {
         return (
             <View>
-                <Button title="Add new Hand" onPress={() => this.addItem() }></Button>
                 <FlatList
                     data={this.state.Session.HandList}
                     renderItem={({item}) => this.HandComponent(item)}
@@ -54,15 +151,55 @@ export default class Session extends React.Component {
                 />
             </View>
         );
+      }
+      else {
+        return (
+          <View></View>
+        );
+      }
     };
     HandComponent = (hand) => {
         return (
           <View>
-            <Button title={'hand ' + hand.Id.toString()}
-              onPress={() => { this.NavigateToNextScreen(hand) }} />
+          <TouchableOpacity 
+            onPress={() => { this.NavigateToNextScreen(hand) }}>
+            <Card>
+              <CardItem>
+                <Text>{'Hand ' + hand.Id.toString()}</Text>
+                <Right>
+                  <View style={styles.horizontal}>
+                    { _Card(hand.MyCards[0]) }
+                    { _Card(hand.MyCards[1]) }
+                  </View>
+                </Right>
+              </CardItem>
+            </Card>
+          </TouchableOpacity>
           </View>
         );
       };
+    LineDivider = () => {
+        if (!this.state.EditMode) {
+          return (
+            <View style={{
+              paddingTop: 5,
+              paddingBottom: 5,
+            }}>
+              <View style={{
+                  borderBottomColor: 'black',
+                  borderBottomWidth: 1,
+              }}
+              />
+              </View>
+          );
+        }
+        else {
+          return (
+            <View>
+            </View>
+          );
+        }
+    };
     UpdateHand = (handToUpdate) => {
         this.setState((state, props) => ({
            ...state,
@@ -93,5 +230,16 @@ const styles = StyleSheet.create({
   },
   style_flex3: {
     flex: 3,
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textStyle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  textInput: {
+    fontSize: 20,
   },
 });
